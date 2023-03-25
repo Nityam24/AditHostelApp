dc6fd052740b9c2d97f800243d6eb6154979e38e
Nityam24 <nityampatel2402@gmail.com>
2023-03-25 17:31:04 +0530

    updated

--- src/component/form.tsx
+++ src/component/form.tsx
@@ -5,16 +5,24 @@ export default function Form(){
     return(
         <form className="complaint-form">
         <h1>ADIT Hostel Maintainance System</h1>
-        <div className="main">
+    <div className="main">
           
         <label>Hostel Block</label>
         <select>
-            <option value="select hostel block">select hostel block</option>
-            <option value=""></option>
+        <option value="select hostel block">select hostel block</option>
+           <option value="ADIT Boys Hostel Block: A">ADIT Boys Hostel Block: A</option>
+          <option value="ADIT Boys Hostel Block: B">ADIT Boys Hostel Block: B</option>
+          <option value="ADIT Boys Hostel Block: 5">ADIT Boys Hostel Block: 5</option>
+          <option value="ADIT Boys Hostel Block: 6">ADIT Boys Hostel Block: 6</option>
+          <option value="ADIT Boys Hostel Block: 7">ADIT Boys Hostel Block: 7</option>
+          <option value="ADIT Boys Hostel Block: 8">ADIT Boys Hostel Block: 8</option>
+          <option value="ADIT Boys Hostel Block: 11">ADIT Boys Hostel Block: 11</option>
+          <option value="Ayurvedic Hut">Ayurvedic Hut</option>
+          <option value="Sharda Girls Hostel">Sharda Girls Hostel</option>
         </select>
 
         <label>Hostel Ractor</label>
-        <input type="text"></input>
+        <input type="text" disabled></input>
 
         <label>Room number</label>
         <input type="text" placeholder="Enter hostel room number "></input>
@@ -45,62 +53,10 @@ export default function Form(){
 
         <label>Your Email</label>
         <input type="tel" placeholder="Enter phone Email"></input>
+    </div>
         <button className="button">Submit Complaint</button>
-      </div>
 
         
         </form>
     )
 }
-// import * as React from 'react';
-// import Box from '@mui/material/Box';
-// import TextField from '@mui/material/TextField';
-// import InputLabel from '@mui/material/InputLabel';
-// import MenuItem from '@mui/material/MenuItem';
-// import FormControl from '@mui/material/FormControl';
-// import Select, { SelectChangeEvent } from '@mui/material/Select';
-
-        
-// export default function FullWidthTextField() {
-//     const [age, setAge] = React.useState('');
-
-//     const handleChange = (event: SelectChangeEvent) => {
-//       setAge(event.target.value = string)
-//     };
-//   return (
-//     <Box
-//       sx={{
-//         width: 500,
-//         maxWidth: '100%',
-//          minWidth: 120 
-//       }}
-      
-//     >
-
-// <FormControl fullWidth>
-//         <InputLabel id="demo-simple-select-label">Age</InputLabel>
-//         <Select
-//           labelId="demo-simple-select-label"
-//           id="demo-simple-select"
-//           value={age}
-//           label="Age"
-//           onChange={handleChange}
-//         >
-//           <MenuItem value={10}>Ten</MenuItem>
-//           <MenuItem value={20}>Twenty</MenuItem>
-//           <MenuItem value={30}>Thirty</MenuItem>
-//         </Select>
-//       </FormControl>
-
-//       <TextField fullWidth label="Enter the hostel room number " id="fullWidth" />
-//       <TextField fullWidth label="Describe your complain in detail" id="fullWidth" />
-//       <TextField fullWidth label="Enter your name" id="fullWidth" />
-//       <TextField fullWidth label="Enter phone number" id="fullWidth" />
-//       <TextField fullWidth label="Enter phone Email" id="fullWidth" />
-//       <TextField fullWidth label="" id="fullWidth" />
-      
-//     </Box>
-//   );
-//     }
-
-    
